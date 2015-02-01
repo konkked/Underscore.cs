252daa9b18ad49c16274122e9ec44a2d6371be31
Charles Keyser <1chkeyser1@gmail.com>
2015-02-01 17:38:07 -0500

    Updating to match nuget package
    removed asm signing, removed asm config attr, updated vbuild

--- Underscore.cs/Properties/AssemblyInfo.cs
+++ Underscore.cs/Properties/AssemblyInfo.cs
@@ -3,10 +3,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Underscore.cs")]
 [assembly: AssemblyDescription("Unified Utility Library Underscore.cs")]
-[assembly: AssemblyConfiguration("Edge")]
 [assembly: AssemblyProduct("Underscore.cs")]
 [assembly: AssemblyCopyright("Copyright © 2014-2015 Chip Keyser")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b96a9e7d-de48-4b0d-8397-d98a7a734880")]
-[assembly: AssemblyVersion("0.1.0.4")]
-[assembly: AssemblyFileVersion("0.1.0.4")]
+[assembly: AssemblyVersion("0.1.0.5")]
+[assembly: AssemblyFileVersion("0.1.0.5")]
