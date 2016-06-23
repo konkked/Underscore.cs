6258e59407f0b2ea4f662ffd3c75ef88bf04d801
zckeyser <zckeyser@gmail.com>
2016-06-23 15:19:25 -0400

    updated Collection.Partition documentation and tests

---
+++ Underscore.Test/Collection/Partition/ChunkTest.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Underscore.Collection;
+using Underscore.Utility;
+
+namespace Underscore.Test.Collection.Partition
+{
+	[TestClass]
+	public class ChunkTest
+	{
+		private int[] target;
+		private PartitionComponent component;
+
+		[TestInitialize]
+		public void Intitialize()
+		{
+			target = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			component = new PartitionComponent(new Underscore.List.PartitionComponent(new MathComponent()));
+		}
+
+		[TestMethod]
+		public void Collection_Partition_Chunk_IndexEvenDistribution()
+		{
+			var expected = new List<List<int>>
+			{
+				new List<int> { 0, 1 },
+				new List<int> { 2, 3 },
+				new List<int> { 4, 5 },
+				new List<int> { 6, 7 },
+				new List<int> { 8, 9 }
+			};
+
+			var result = component.Chunk(target, 2).ToList();
+
+			Assert.AreEqual(expected.Count, result.Count);
+
+			// for each chunk, make sure it's sequentially equal 
+			// to its respective expected chunk
+			for (var i = 0; i < result.Count; i++)
+			{
+				Assert.IsTrue(expected[i].SequenceEqual(result[i]));
+			}
+		}
+
+		[TestMethod]
+		public void Collection_Partition_Chunk_IndexUnevenDistribution()
+		{
+			var expected = new List<List<int>>
+			{
+				new List<int> { 0, 1, 2 },
+				new List<int> { 3, 4, 5 },
+				new List<int> { 6, 7, 8 },
+				new List<int> { 9 }
+			};
+
+			var result = component.Chunk(target, 3).ToList();
+
+			Assert.AreEqual(expected.Count, result.Count);
+
+			// for each chunk, make sure it's sequentially equal 
+			// to its respective expected chunk
+			for (var i = 0; i < result.Count; i++)
+			{
+				Assert.IsTrue(expected[i].SequenceEqual(result[i]));
+			}
+		}
+
+		[TestMethod]
+		public void Collection_Partition_Chunk_PredicateNoMatches()
+		{
+			var expected = new List<List<int>>
+			{
+				new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+			};
+
+			var result = component.Chunk(target, n => n == 10).ToList();
+
+			Assert.AreEqual(expected.Count, result.Count);
+
+			// for each chunk, make sure it's sequentially equal 
+			// to its respective expected chunk
+			for (var i = 0; i < result.Count; i++)
+			{
+				Assert.IsTrue(expected[i].SequenceEqual(result[i]));
+			}
+		}
+
+		[TestMethod]
+		public void Collection_Partition_Chunk_PredicateSomeMatches()
+		{
+			var expected = new List<List<int>>
+			{
+				new List<int> { 0, 1 },
+				new List<int> { 2, 3 },
+				new List<int> { 4, 5 },
+				new List<int> { 6, 7 },
+				new List<int> { 8, 9 }
+			};
+
+			var result = component.Chunk(target, n => n % 2 == 0).ToList();
+
+			Assert.AreEqual(expected.Count, result.Count);
+
+			// for each chunk, make sure it's sequentially equal 
+			// to its respective expected chunk
+			for (var i = 0; i < result.Count; i++)
+			{
+				Assert.IsTrue(expected[i].SequenceEqual(result[i]));
+			}
+		}
+
+		[TestMethod]
+		public void Collection_Partition_Chunk_PredicateAllMatches()
+		{
+			var expected = new List<List<int>>
+			{
+				new List<int> { 0 },
+				new List<int> { 1 },
+				new List<int> { 2 },
+				new List<int> { 3 },
+				new List<int> { 4 },
+				new List<int> { 5 },
+				new List<int> { 6 },
+				new List<int> { 7 },
+				new List<int> { 8 },
+				new List<int> { 9 },
+			};
+
+			var result = component.Chunk(target, n => n % 1 == 0).ToList();
+
+			Assert.AreEqual(expected.Count, result.Count);
+
+			// for each chunk, make sure it's sequentially equal 
+			// to its respective expected chunk
+			for (var i = 0; i < result.Count; i++)
+			{
+				Assert.IsTrue(expected[i].SequenceEqual(result[i]));
+			}
+		}
+	}
+}
---
+++ Underscore.Test/Collection/Partition/PartitionTest.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Underscore.Collection;
+using Underscore.Utility;
+
+namespace Underscore.Test.Collection
+{
+	[TestClass]
+	public class PartitionTest
+	{
+		private PartitionComponent component;
+		private int[] target;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			component = new PartitionComponent(new Underscore.List.PartitionComponent(new MathComponent()));
+			target = new[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		}
+
+		[TestMethod]
+		public void Collection_Partition_Partition_IndexStartOfList()
+		{
+			var expected = Tuple.Create(
+				new List<int> (),
+				new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+			);
+
+			var result = component.Partition(target, 0);
+
+			Assert.IsTrue(expected.Item1.SequenceEqual(result.Item1));
+			Assert.IsTrue(expected.Item2.SequenceEqual(result.Item2));
+		}
+
+		[TestMethod]
+		public void Collection_Partition_Partition_IndexMiddleOfList()
+		{
+			var expected = Tuple.Create(
+				new List<int> { 0, 1, 2, 3, 4 },
+				new List<int> { 5, 6, 7, 8, 9 }
+			);
+
+			var result = component.Partition(target, 5);
+
+			Assert.IsTrue(expected.Item1.SequenceEqual(result.Item1));
+			Assert.IsTrue(expected.Item2.SequenceEqual(result.Item2));
+		}
+
+		[TestMethod]
+		public void Collection_Partition_Partition_IndexAtEndOfList()
+		{
+			var expected = Tuple.Create(
+				new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8 },
+				new List<int> { 9 }
+			);
+
+			var result = component.Partition(target, 9);
+
+			Assert.IsTrue(expected.Item1.SequenceEqual(result.Item1));
+			Assert.IsTrue(expected.Item2.SequenceEqual(result.Item2));
+		}
+
+		[TestMethod]
+		public void Collection_Partition_Partition_IndexPastEndOfList()
+		{
+			var expected = Tuple.Create(
+				new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+				new List<int>()
+			);
+
+			var result = component.Partition(target, 10);
+
+			Assert.IsTrue(expected.Item1.SequenceEqual(result.Item1));
+			Assert.IsTrue(expected.Item2.SequenceEqual(result.Item2));
+		}
+
+		[TestMethod]
+		public void Collection_Partition_Partition_PredicateNoMatch()
+		{
+			var expected = Tuple.Create(
+				new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+				new List<int>()
+			);
+
+			var result = component.Partition(target, n => n == 10);
+
+			Assert.IsTrue(expected.Item1.SequenceEqual(result.Item1));
+			Assert.IsTrue(expected.Item2.SequenceEqual(result.Item2));
+		}
+
+		[TestMethod]
+		public void Collection_Partition_Partition_PredicateHasMatch()
+		{
+			var expected = Tuple.Create(
+				new List<int> { 0, 1, 2, 3, 4 },
+				new List<int> { 5, 6, 7, 8, 9 }
+			);
+
+			var result = component.Partition(target, n => n == 5);
+
+			Assert.IsTrue(expected.Item1.SequenceEqual(result.Item1));
+			Assert.IsTrue(expected.Item2.SequenceEqual(result.Item2));
+		}
+
+		[TestMethod]
+		public void Collection_Partition_PartitionMatches_NoMatches()
+		{
+			var expected = Tuple.Create(
+				new List<int>(),
+				new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+			);
+
+			var result = component.PartitionMatches(target, n => n == 10);
+
+			Assert.IsTrue(expected.Item1.SequenceEqual(result.Item1));
+			Assert.IsTrue(expected.Item2.SequenceEqual(result.Item2));
+		}
+
+		[TestMethod]
+		public void Collection_Partition_PartitionMatches_SomeMatches()
+		{
+			var expected = Tuple.Create(
+				new List<int> { 0, 2, 4, 6, 8 },
+				new List<int> { 1, 3, 5, 7, 9 }
+			);
+
+			var result = component.PartitionMatches(target, n => n % 2 == 0);
+
+			Assert.IsTrue(expected.Item1.SequenceEqual(result.Item1));
+			Assert.IsTrue(expected.Item2.SequenceEqual(result.Item2));
+		}
+
+		[TestMethod]
+		public void Collection_Partition_PartitionMatches_AllMatches()
+		{
+			var expected = Tuple.Create(
+				new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+				new List<int>()
+			);
+
+			var result = component.PartitionMatches(target, n => n % 1 == 0);
+
+			Assert.IsTrue(expected.Item1.SequenceEqual(result.Item1));
+			Assert.IsTrue(expected.Item2.SequenceEqual(result.Item2));
+		}
+	}
+}
