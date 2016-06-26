be4d2837aeff83a0a1240db82fe0dda653a7908e
zckeyser <zckeyser@gmail.com>
2016-06-26 02:41:41 -0400

    added Utility.Math tests and docs

--- Underscore.Test/Utility/MathTest.cs
+++ Underscore.Test/Utility/MathTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using Underscore.Utility;
 
@@ -7,45 +8,255 @@ namespace Underscore.Test.Utility
     [TestClass]
     public class MathTest
     {
-        [TestMethod]
-        public void MathCtor( )
+        private MathComponent component;
+
+        [TestInitialize]
+        public void Initialize()
         {
-            var component = new MathComponent( );
+            component = new MathComponent();
         }
 
-
         [TestMethod]
-        public void MathRandom( )
+        public void Utility_Math_Random_NoArguments()
         {
-            var component = new MathComponent( );
-            //just check if works, randomness will have to be tested later
-            var rand = component.Random( );
+            var result = component.Random();
 
-            var rand2 = component.Random( 10 );
-
-            Assert.IsTrue( rand2 <= 10 );
-
-            var rand3 = component.Random( 10, 20 );
-
-            Assert.IsTrue( rand3 >= 10 && rand3 <= 20 );
-
+            Assert.IsTrue(result >= 0 && result < int.MaxValue);
         }
 
+        [TestMethod]
+        public void Utility_Math_Random_1Argument_Positive()
+        {
+            var result = component.Random(100);
+
+            Assert.IsTrue(result >= 0 && result < 100);
+        }
 
         [TestMethod]
-        public void UtilityUnique( )
+        public void Utility_Math_Random_1Argument_Zero_ThrowsException()
         {
-            var component = new MathComponent( );
+            var didThrow = false;
 
-            var output = component.UniqueId( "prefix" );
-            Assert.IsTrue( output.StartsWith( "prefix" ) );
+            try
+            {
+                component.Random(0);
+            }
+            catch (ArgumentException)
+            {
+                didThrow = true;
+            }
 
+            Assert.IsTrue(didThrow);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Random_1Argument_Negative_ThrowsException()
+        {
+            var didThrow = false;
+
+            try
+            {
+                component.Random(-100);
+            }
+            catch (ArgumentException)
+            {
+                didThrow = true;
+            }
+
+            Assert.IsTrue(didThrow);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Random_2Arguments_NegativeMinPositiveMax()
+        {
+            var result = component.Random(-100, 100);
+
+            Assert.IsTrue(result >= -100 && result < 100);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Random_2Arguments_NegativeMinNegativeMax()
+        {
+            var result = component.Random(-100, -30);
+
+            Assert.IsTrue(result >= -100 && result < -30);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Random_2Arguments_PositiveMinPositiveMax()
+        {
+            var result = component.Random(30, 100);
+
+            Assert.IsTrue(result >= 30 && result < 100);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Random_2Arguments_MinGreaterThanMax_ThrowsException()
+        {
+            var didThrow = false;
+
+            try
+            {
+                component.Random(100, -100);
+            }
+            catch (ArgumentException)
+            {
+                didThrow = true;
+            }
+
+            Assert.IsTrue(didThrow);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Random_2Arguments_MinEqualToMax_ThrowsException()
+        {
+            var didThrow = false;
+
+            try
+            {
+                component.Random(100, 100);
+            }
+            catch (ArgumentException)
+            {
+                didThrow = true;
+            }
+
+            Assert.IsTrue(didThrow);
+        }
+
+        [TestMethod]
+        public void Utility_Math_UniqueId_IsUnique()
+        {
             var hashset = new HashSet<string>();
 
-            for ( int i=0 ; i < 30 ; i++ )
-                Assert.IsTrue( hashset.Add( component.UniqueId( ) ) );
-
+            // the chances of a guid collision in a data set this size
+            // are infintisemally small if the guid is being generated correctly
+            for (int i = 0; i < 1000; i++)
+                Assert.IsTrue(hashset.Add(component.UniqueId())); 
         }
 
+        [TestMethod]
+        public void Utility_Math_UniqueId_IsPrefixed()
+        {
+            var output = component.UniqueId("prefix");
+            Assert.IsTrue(output.StartsWith("prefix"));
+        }
+
+        [TestMethod]
+        public void Utility_Math_Abs_NegativeInput()
+        {
+            const int expected = 50;
+            var result = component.Abs(-50);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Abs_PositiveInput()
+        {
+            const int expected = 50;
+            var result = component.Abs(50);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Abs_ZeroInput()
+        {
+            const int expected = 0;
+            var result = component.Abs(0);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Min_LeftSmaller()
+        {
+            const int expected = 1;
+            var result = component.Min(1, 10);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Min_RightSmaller()
+        {
+            const int expected = 1;
+            var result = component.Min(10, 1);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Min_BothNegative()
+        {
+            const int expected = -10;
+            var result = component.Min(-10, -1);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Min_MixedNegativePositive()
+        {
+            const int expected = -10;
+            var result = component.Min(-10, 1);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Min_BothPositive()
+        {
+            const int expected = 1;
+            var result = component.Min(10, 1);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Max_LeftSmaller()
+        {
+            const int expected = 10;
+            var result = component.Max(1, 10);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Max_RightSmaller()
+        {
+            const int expected = 10;
+            var result = component.Max(10, 1);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Max_BothNegative()
+        {
+            const int expected = -1;
+            var result = component.Max(-10, -1);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Max_MixedNegativePositive()
+        {
+            const int expected = 1;
+            var result = component.Max(-10, 1);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Utility_Math_Max_BothPositive()
+        {
+            const int expected = 10;
+            var result = component.Max(10, 1);
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
--- docs/api/Utility.md
+++ docs/api/Utility.md
@@ -17,3 +17,53 @@ _.IsTruthy("something"); // true
 _.IsTruthy(false); // false
 _.IsTruthy(true); // true
 ```
+
+## Math
+### string UniqueId(string prefix)
+Returns a GUID (Globally Unique Identifier) prefixed with the given string and "\_"
+```
+Console.WriteLine(_.Utility.UniqueId("foobar")); // looks like foobar_F9168C5E-CEB2-4FAA-B6BF-329BF39FA1E4
+```
+
+### string UniqueId(string prefix)
+Returns a GUID (Globally Unique Identifier)
+```
+Console.WriteLine(_.Utility.UniqueId()); // looks like F9168C5E-CEB2-4FAA-B6BF-329BF39FA1E4
+```
+
+### int Random(int min, int max)
+Generates a random integer between the given min and max, where min is inclusive and max is exclusive.
+```
+Console.WriteLine(_.Random(-100, 100)) // can be any integer in [-100..99]
+```
+
+### int Random(int max)
+Generates a random integer between 0 and the given max, where max is exclusive.
+```
+Console.WriteLine(_.Random(100)); // can be any integer in [0..99]
+```
+
+### int Random()
+Generates a random integer between 0 and Int32.MaxValue, max bound is exclusive
+```
+Console.WriteLine(_.Random()); // can be any integer in [0..Integer.MaxValue - 1]
+```
+
+### int Abs(int i)
+Returns the absolute value of the given value (performant implementation).
+```
+Console.WriteLine(_.Abs(-100)); // 100
+Console.WriteLine(_.Abs(100)); // 100
+```
+
+### int Min(int i)
+Returns the smaller of the two given values (performant implementation).
+```
+Console.WriteLine(_.Min(1, 10)); // 1
+```
+
+### int Max(int i)
+Returns the larger of the two given values (performant implementation).
+```
+Console.WriteLine(_.Max(1, 10)); // 10
+```
