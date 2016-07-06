b1770627282dc3707f5f10595490e75fd87ef90c
Chris Keyser <ckeyser@emoneyadvisor.com>
2016-07-06 15:20:20 -0400

    finished case matching implementations and tests

--- Underscore.cs/Utility/Implementation/String.cs
+++ Underscore.cs/Utility/Implementation/String.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Policy;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Underscore.Utility
 {
@@ -12,7 +9,27 @@ namespace Underscore.Utility
 	{
 		public string ToCamelCase(string s)
 		{
-			throw new NotImplementedException();
+			// convert it to snake_case as a baseline
+			var toConvert = Normalize(s);
+			var chars = toConvert.ToCharArray();
+			var output = "";
+
+			for (var i = 0; i < chars.Length; i++)
+			{
+				if (chars[i] == '_')
+				{
+					output += chars[i + 1].ToString().ToUpper();
+
+					// skip the next character since we added it already
+					i++;
+				}
+				else
+				{
+					output += chars[i];
+				}
+			}
+
+			return output;
 		}
 
 		public bool IsCamelCase(string s)
@@ -25,7 +42,10 @@ namespace Underscore.Utility
 
 		public string ToPascalCase(string s)
 		{
-			throw new NotImplementedException();
+			// convert it to snake_case as a baseline
+			var toConvert = Normalize(s);
+
+			return toConvert.Substring(0, 1).ToUpper() + ToCamelCase(toConvert.Substring(1));
 		}
 
 		public bool IsPascalCase(string s)
@@ -54,7 +74,59 @@ namespace Underscore.Utility
 
 		public string ToSnakeCase(string s)
 		{
-			throw new NotImplementedException();
+			if (IsSnakeCase(s))
+				return s;
+			if (IsCamelCase(s))
+				return CamelToSnake(s);
+			if (IsPascalCase(s))
+				return PascalToSnake(s);
+			if (IsKebabCase(s))
+				return KebabToSnake(s);
+			
+			return OtherToSnake(s);
+		}
+
+		private string CamelToSnake(string s)
+		{
+			var output = "";
+			var chars = s.ToCharArray();
+
+			foreach (var c in chars)
+			{
+				if (IsUpper(c))
+				{
+					output += "_" + c.ToString().ToLower();
+				}
+				else
+					output += c;
+			}
+
+			return output;
+		}
+
+		private string PascalToSnake(string s)
+		{
+			// take off first char since it's supposed to be capitalized
+			var output = s.Substring(0, 1).ToLower();
+
+			// treat the rest like it's camelCase since the only difference is the first letter
+			return output + CamelToSnake(s.Substring(1));
+		}
+
+		private string KebabToSnake(string s)
+		{
+			// only difference is that kebab uses - instead of _
+			return s.Replace('-', '_');
+		}
+
+		private string OtherToSnake(string s)
+		{
+			// since this isn't a normally defined type, we're going to do the following:
+			// make it all lowercase
+			// treat ' ', '.', '-', ',', '/', and '\' as delimiters
+			const string pattern = @"[.\-,/\\\s]";
+			var regex = new Regex(pattern);
+			return regex.Replace(s.ToLower(), "_");
 		}
 
 		public bool IsSnakeCase(string s)
@@ -67,7 +139,8 @@ namespace Underscore.Utility
 
 		public string ToKebabCase(string s)
 		{
-			throw new NotImplementedException();
+			// normalize to snake case and replace underscores with hyphens
+			return Normalize(s).Replace('_', '-');
 		}
 
 		public bool IsKebabCase(string s)
@@ -104,6 +177,33 @@ namespace Underscore.Utility
 			const string pattern = @"^[a-zA-Z]$";
 
 			return Regex.IsMatch(c.ToString(), pattern);
+		}
+
+		public bool IsLower(char c)
+		{
+			const string pattern = @"^[a-z]$";
+
+			return Regex.IsMatch(c.ToString(), pattern);
+		}
+
+		public bool IsUpper(char c)
+		{
+			const string pattern = @"^[A-Z]$";
+
+			return Regex.IsMatch(c.ToString(), pattern);
+		}
+
+		/// <summary>
+		/// normalize a string to snake_case, 
+		/// that way we only need to write
+		/// one conversion for every other casing
+		/// 
+		/// snake case was chosen because snake and kebab case
+		/// are the easiest to convert out of
+		/// </summary>
+		private string Normalize(string s)
+		{
+			return ToSnakeCase(s);
 		}
 	}
 }
